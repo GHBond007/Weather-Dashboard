e23b5ba1a56b39bc948f80c6b32a3cfc0d488f4f
GHBond007 <121248438+GHBond007@users.noreply.github.com>
2023-03-12 14:35:16 -0500

    New js html

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,6 @@ const forecast = document.getElementById('forecast');
 // Define the API key for OpenWeatherMap
 const apiKey = '540da406fc27f9b6e16fd51ec5cded3b';
 
-const pastSearches = document.getElementById('pastSearches');
 
 
 // Add an event listener to the search button
@@ -69,31 +68,6 @@ searchButton.addEventListener('click', () => {
       alert('An error occurred while fetching weather data');
       console.error(error);
     });
-    if (localStorage["pastSearches"]) {
-  pastSearches = JSON.parse(localStorage["pastSearches"]);
-  
-}
-if (pastSearches.indexOf(search) ==-1) {
-  pastSearches.unshift(search);
-  if (pastSearches.length > 5) {
-    pastSearches.pop();
     
-  }
-  localStorage["pastSearches"] =JSON.stringify(pastSearches);
-}
-
-function drawPastSearches() {
-  if (pastSearches.length) {
-    var html = pastSearchesTemplate({search:pastSearches});
-    $("#pastSearches").html(html);
-    
-  }
-  
-}
-
-$(document).on("click", ".pastSearchLink" , function(e) {
-  e.preventDefault();
-  var search = $(this).text();
-  dosearch(search);
-});
+    localStorage.setItem("forecastData", JSON.stringify(cityInput));
 });
